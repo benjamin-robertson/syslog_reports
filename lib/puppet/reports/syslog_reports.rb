a79dc762e516e4ee26b3d6c1cc8d499a6a1808bd
Benjamin Robertson <ben.robertson@puppet.com>
2021-07-14 14:22:07 +1000

    Udapted code

--- lib/puppet/reports/syslog_reports.rb
+++ lib/puppet/reports/syslog_reports.rb
@@ -2,13 +2,13 @@ require 'puppet'
 require 'syslog/logger'
 
 Puppet::Reports.register_report(:syslog_reports) do
-  desc "Send the corrective changes to syslog"
+  desc 'Send the corrective changes to syslog'
 
-  desc "Setup logger"
+  desc 'Setup logger'
   # logdest = Syslog::Logger.new 'PuppetReports'
 
   def process
     Syslog.log(Syslog::Log_puppet, self)
-    #logdest.info(self)
+    # logdest.info(self)
   end
 end
